4c823db515c2f8414d06bdb08da86dcf8772d8c4
Orif-92 <sarniyozovorif@gmail.com>
2023-08-11 15:38:06 +0500

    reverse int

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function reverse(number) {
+  const reversedString = String(number).split('').reverse().join('');
+  return Number(reversedString);
+}
+
+module.exports = reverse;
+
+console.log(reverse(123));    
+console.log(reverse(233));    
+console.log(reverse(535));    
+console.log(reverse(95034));  
